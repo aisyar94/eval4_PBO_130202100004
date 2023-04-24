42feec5e024a8e60a02d8560612383fdfbb468f2
aisyar94 <127504299+aisyar94@users.noreply.github.com>
2023-04-24 07:15:11 -0700

    Add files via upload

---
+++ nomor1/Mahasiswa.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.nomor1_eval4_13020210004;
+
+/**
+ *
+ * @author ACER
+ */
+public class Mahasiswa {
+    private String nama;
+    private int umur;
+    private String jurusan;
+    
+    public String getNama() {
+        return nama;
+    }
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    public int getUmur() {
+        return umur;
+    }
+    
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+    
+    public String getJurusan() {
+        return jurusan;
+    }
+    
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+}
+
+
---
+++ nomor1/Nomor1_eval4_13020210004.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nomor1_eval4_13020210004;
+
+/**
+ *
+ * @author ACER
+ */
+public class Nomor1_eval4_13020210004 {
+
+    public static void main(String[] args) {
+       
+        Mahasiswa mhs = new Mahasiswa();
+        
+        mhs.setNama("Aisyah");
+        mhs.setUmur(20);
+        mhs.setJurusan("Teknik Informatika");
+        System.out.println("Nama: " + mhs.getNama());
+        System.out.println("Umur: " + mhs.getUmur());
+        System.out.println("Jurusan: " + mhs.getJurusan());
+
+    }
+}
