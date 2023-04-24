a1d35845d006e4c91465fd0f75dc7a7d64207798
aisyar94 <127504299+aisyar94@users.noreply.github.com>
2023-04-24 06:14:35 -0700

    Add files via upload

---
+++ Burung.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.modul4_hewan_13020210004;
+
+/**
+ *
+ * @author ACER
+ */
+public class Burung extends Hewan {
+    public void makan() {
+        System.out.println("Burung makan biji-bijian");
+    }
+}
+
+
---
+++ Kucing.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.modul4_hewan_13020210004;
+
+/**
+ *
+ * @author ACER
+ */
+    public class Kucing extends Hewan {
+        
+    public void makan() {
+        System.out.println("Kucing makan ikan");
+    }
+}
+
